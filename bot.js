a016be62088a1d207b52482a1603ee188cef0931
rcamenzind05 <rcamenzind05@gmail.com>
2021-03-09 19:16:19 -0700

    forgot 1 thing lmao

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ client.on("ready", () => {
     console.log("LOL ONIONS!");
     console.log("Initializing...");
     // ANCHOR: init code here if needed
-    client.user.setActivity("test", { type: 'WATCHING' });
+    client.user.setActivity("Blan's Birthday!", { type: 'STREAMING' });
     console.log("Initialization Complete.");
 });
 client.on("message", (message) => {
